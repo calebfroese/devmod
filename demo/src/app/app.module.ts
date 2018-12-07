6e58301614e04d1b6f867c84fc1e6421a35b01eb
Caleb Froese <cfroese@nextfaze.com>
2018-12-07 15:03:05 +1030

    Add prod config to devmod demo

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -10,14 +10,20 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { DevmodInterfaceModule } from '@devmod/interface';
+import { DevmodInterfaceModule, DevmodNoopModule } from '@devmod/interface';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { DashComponent } from './dash/dash.component';
 import { LoginComponent } from './login/login.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UsersService } from './users.service';
+
+let devmod = DevmodNoopModule; // Use 'noop' module in production so we don't see devmod toggle
+if (!environment.production) {
+  devmod = DevmodInterfaceModule; // Use real devmod module in development
+}
 
 @NgModule({
   declarations: [
@@ -28,9 +34,9 @@ import { UsersService } from './users.service';
     LoginComponent
   ],
   imports: [
+    devmod,
     BrowserAnimationsModule,
     BrowserModule,
-    DevmodInterfaceModule,
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
--- demo/src/devmod.config.ts
+++ demo/src/devmod.config.ts
@@ -1,3 +1,6 @@
 import { enableDebugMode } from '@devmod/core';
+import { environment } from './environments/environment';
 
-enableDebugMode();
+if (!environment.production) {
+  enableDebugMode(); // Ensures all the decorators do what they should
+}
